28661af42b1fd3aa9077edb1304420d559e06f11
Nilay Savant <nilayofficial@gmail.com>
2019-03-26 21:34:19 +0530

    Added imdb-api and fs,  fs to get APi key from file instead of defining directly. Note that the API file is gitignored obviously

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const imdb = require('imdb-api')
+const fs = require('fs')
 
 
-const PORT = 8080
-const app = express()
+const PORT = 8080 // Server Port Variable
+
+const getApiKey = () => {
+    const api_file = fs.readFileSync('OMDB_API_KEY.txt')
+    return api_file.toString()
+}
+const OMDB_API_KEY = getApiKey() // get api key from file
+console.log('OMDB API KEY:', OMDB_API_KEY)
+
+const app = express() // init express app
 
 app.use(morgan('dev')) // a logger for express
 
 // to parse the body of post requests -> bodyParser :
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
